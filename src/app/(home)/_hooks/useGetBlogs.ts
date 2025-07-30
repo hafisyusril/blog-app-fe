f182c111e9c346e0524f5912488d04b8ebbf5548
YUS / PIS <yusrilhafis@gmail.com>
2025-07-30 14:14:34 +0700

    feat: bloglist

--- src/app/(home)/_hooks/useGetBlogs.ts
+++ src/app/(home)/_hooks/useGetBlogs.ts
@@ -1,14 +1,17 @@
 import { axiosInstance } from "@/lib/axios";
 import { Blog } from "@/types/blog";
+import { PageableRespones, PagiantionQueries } from "@/types/pagination";
 import { useQuery } from "@tanstack/react-query";
 
-const useGetBlogs = () => {
+interface GetBlogsQuery extends PagiantionQueries {
+  search?: string
+}
+
+const useGetBlogs = (queries?: GetBlogsQuery) => {
   return useQuery({
-    queryKey: ["blogs"],
+    queryKey: ["blogs", queries],
     queryFn: async () => {
-      const { data } = await axiosInstance.get<Blog[]>(
-        "/api/data/Blogs?sortBy=%60created%60%20desc"
-      );
+      const { data } = await axiosInstance.get<PageableRespones<Blog>>("/blogs", {params: queries});
       return data;
     },
   });
