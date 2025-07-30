5121fd1251b6d41c9c7e20175b4a739bd0da4ad0
YUS / PIS <49065681+hafisyusril@users.noreply.github.com>
2025-07-30 15:21:41 +0700

    feat: blog detail (#3)

---
+++ src/app/blogs/[slug]/error.tsx
@@ -0,0 +1,10 @@
+"use client"
+
+const Error = () => {
+    return (
+        <div className="flex items-center  justify-center font-bold text-3xl h-screen">
+            Something went Wrong
+        </div>
+    )
+}
+export default Error
